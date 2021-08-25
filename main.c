5f381490236034a76fd44a25c61fd1c90e5c0c08
v1ctor13 <victorviniciuspj@gmail.com>
2021-08-24 22:18:41 -0300

    initial commit

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <GLFW\glfw3.h>
+
+int main(int argv, const char* argc[]){
+	
+	if(glfwInit()){
+		printf("parece q deu bom\n");
+	}
+	
+	
+	return 0;
+}
